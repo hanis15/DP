c160050dfc3b5ad9abf57b501e066ab41df60b24
Jaroslav Hanak <Jaroslav Hanak>
2016-03-21 18:24:51 +0100

    Backup show CZ map with geoJSON

---
+++ test_14_geoJSON_grah/source/main.js
@@ -0,0 +1,103 @@
+// JavaScript source code
+
+function Graph(id, width, height) {
+    this.create_graph = load_graph;
+
+    function load_graph() {
+        d3.json("draft_v1.json", parse_data);
+    }
+
+    function parse_data(error, data) {
+        if (error) throw error;
+
+        force = d3.layout.force()
+            .charge(10)
+            .linkDistance(200)
+            .size([width, height]);
+
+        svg = d3.select(id).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("style", "border: 1px solid black;");
+
+        force
+            .nodes(data.networks)
+            .links(data.routes)
+            .start();
+
+        link = svg.selectAll(".link")
+            .data(data.routes)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function (d) { return d.bandwidth / 100; })
+            .attr("title", function (d) { return d.name; })
+            .attr("id", function (d) { return d.name; });
+
+        node = svg.selectAll(".node")
+            .data(data.networks)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", 20)
+            .attr("fill", function(d) { return "red"; })
+            .call(force.drag);
+
+        node.append("title")
+            .text(function (d) {
+                return d.name;
+            });
+
+        $("svg line").qtip({
+            content: {
+                button: 'Close',
+                title: function (event, api) {
+                    return api.elements.target.attr("title");
+                },
+                text: $('<iframe height="180" width="640" src="data.html" />')
+            },
+                /*{
+                button: 'Close',
+                title: function (event, api) {
+                    return api.elements.target.attr("title");
+                },
+                    /*
+                text: function (event, api) {
+                    $.get({
+                        url: 'data.html' // Use href attribute as URL
+                    })
+                    .then(function (content) {
+                        // Set the tooltip content upon successful retrieval
+                        api.set('content.text', content);
+                    }, function (xhr, status, error) {
+                        // Upon failure... set the tooltip content to error
+                        api.set('content.text', status + ': ' + error);
+                    });
+                    return "Lodaing :)...";
+                }
+            },*/
+            show: { solo: true },
+            hide: 'unfocus',
+            position: {
+                my: 'left top',
+                at: 'right top',
+                adjust: {
+                    screen: true
+                }
+            },
+            style: {
+                classes: 'qtip-bootstrap',
+                width: 660,
+                height: 230
+            }
+        });
+
+        force.on("tick", function () {
+            link.attr("x1", function (d) { return d.source.x; })
+                .attr("y1", function (d) { return d.source.y; })
+                .attr("y2", function (d) { return d.target.y; })
+                .attr("x2", function (d) { return d.target.x; });
+
+            node.attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; });
+        });
+    }
+}
---
+++ test_14_geoJSON_grah/source/main_Leaflet.js
@@ -0,0 +1,47 @@
+// JavaScript source code
+
+function geo_Leaflet_graph(id, width, height) {
+    this.create_map = load_graph;
+
+    // vytvori mapu
+    function load_graph() {
+        
+        d3.json("json/geoJSON.json", parse_data);
+    }
+
+    function parse_data(error, data) {
+        if (error) throw error;
+        
+        var x_data = [];
+        var y_data = [];
+        for (var count = 0; count < data.features.length; count++) {
+            if (data.features[count].geometry.type == "Point") {
+                x_data.push(data.features[count].geometry.coordinates[0]);
+                y_data.push(data.features[count].geometry.coordinates[1]);
+            }
+        }
+
+        // zobrazeni mapy s ohledem na souradnice bodu
+        var mymap = L.map(id).setView([d3.median(y_data), d3.median(x_data)], 13);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+
+
+        // zobrazeni geoJSON objektu na mape
+        var geojsonMarkerOptions = {
+            radius: 10,
+            fillColor: "red",
+            color: "black",
+            weight: 1
+        };
+
+        L.geoJson(data.features, {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            }
+        }).addTo(mymap);
+        
+    }
+}
