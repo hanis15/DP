ebd0fd0f9c15a3e4f09fca68c93e3871dd7ba19d
Jaroslav Hanak <Jaroslav Hanak>
2016-04-03 19:58:29 +0200

    Add type node and img node and load file with button

--- source/main.js
+++ source/main.js
@@ -1,24 +1,68 @@
 // JavaScript source code
 
 function Graph(id, width, height) {
+    var svg;
     this.create_graph = load_graph;
+    this.define_style = define_style;
+    this.set_data = nacti_data;
+
+    // definovani css style
+    function define_style() {
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = ".ui-tooltip, .qtip{"
+                        + "position: absolute;"
+                        + "left: -28000px;"
+                        + "top: -28000px;"
+                        + "display: none;"
+                        + "max-width: 900px;"
+                        + "min-width: 600px;"
+                        + "font-size: 10.5px;"
+                        + "line-height: 12px;"
+                        + "}";
+        document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
+    function nacti_data(data) {
+        var json_data = jQuery.parseJSON(data);
+
+        svg.remove();
+        new_graph(json_data);
+    }
 
     function load_graph() {
-        d3.json("json/brno_school.geo.json", parse_data);
+        define_style();
+        //d3.json("json/brno_school.geo.json", parse_data);
+        d3.json("json/cz_city.geo.json", parse_data);
     }
 
     function parse_data(error, data) {
         if (error) throw error;
 
-        force = d3.layout.force()
-            .charge(10)
-            .linkDistance(200)
-            .size([width, height]);
+        new_graph(data);
+    }
 
-        svg = d3.select(id).append("svg")
+
+    function new_graph(data) {
+        force = d3.layout.force()
+            .charge(-400)       // zaporne hodnoty nastavuji jak se odpuzuji jednotlivy uzly
+            .linkDistance(200)
+            .size([width, height])
+            .on("tick", tick)
+            .on("end", function () {
+                node.each(function (d) {
+                    d.fixed = true;
+                });
+            });
+
+        svg = d3.select(id)
+            .style("width", width)
+            .style("height", height)
+            .append("svg")
             .attr("width", width)
             .attr("height", height)
-            .attr("style", "border: 1px solid black;");
+            .attr("style", "border: 1px solid black;")
+            .on("click", explicitlyPosition);
 
         // prevod z geoJson do force reprezentace
         nodes = [];
@@ -50,22 +94,35 @@ function Graph(id, width, height) {
             .data(links)
             .enter().append("line")
             .attr("class", "link")
-            //.style("stroke-width", function (d) { return d.bandwidth / 100; })
+            .style("stroke", "#9ecae1")
+            .style("stroke-width", "1.5px")
             .attr("title", function (d) { return d.name; })
             .attr("id", function (d) { return d.name; });
 
-        node = svg.selectAll(".node")
-            .data(nodes)
-            .enter().append("circle")
-            .attr("class", "node")
+        node_g = svg.selectAll("g.node")
+            .data(nodes, function(d) { return d.name; });
+            
+        node = node_g.enter().append("g")
+                             .attr("class", "node")
+                             .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
+                             .call(force.drag);
+
+        node_c = node.append("circle")
             .attr("r", 20)
-            .attr("fill", function(d) { return "red"; })
-            .call(force.drag);
+            .style("fill", "#eee");            
+
+        node.append("image")
+            .attr("xlink:href", function (d) { return (d.type_node == "R") ? "img/router.png" : "img/sensor.png" })
+            .attr("x", function (d) { return -25; })
+            .attr("y", function (d) { return -25; })
+            .attr("height", 50)
+            .attr("width", 50);
 
         node.append("title")
             .text(function (d) {
                 return d.name;
             });
+
 
         $("svg line").qtip({
             content: {
@@ -91,14 +148,28 @@ function Graph(id, width, height) {
             }
         });
 
-        force.on("tick", function () {
+        function explicitlyPosition() {
+            node_c.each(function (d) {
+                d.x = 0;
+                d.y = 0;
+                d.fixed = false;
+            });
+            tick();
+            node_c.each(function (d) {
+                d.fixed = true;
+            });
+            force.resume();
+        }
+
+        function tick() {
             link.attr("x1", function (d) { return d.source.x; })
                 .attr("y1", function (d) { return d.source.y; })
                 .attr("y2", function (d) { return d.target.y; })
                 .attr("x2", function (d) { return d.target.x; });
 
             node.attr("cx", function (d) { return d.x; })
-                .attr("cy", function (d) { return d.y; });
-        });
+                .attr("cy", function (d) { return d.y; })
+                .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+        }
     }
 }
